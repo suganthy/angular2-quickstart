ab98b631c93ce80c76a4ae9f95e1c49dadbe71c2
Suganthy <suganthy876@gmail.com>
2018-03-17 21:12:46 +0530

    Initial commit

---
+++ app/events-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'events-app',
+  template: `<div>Hello {{ angular }}</div>`
+})
+export class EventsAppComponent {
+	angular = "Angular 2";
+}
